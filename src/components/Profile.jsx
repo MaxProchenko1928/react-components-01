62e2d264f62428417f72ff33d220a7cd678f5692
Protsenko Maksym <procenkomaksim762@gmail.com>
2023-07-01 11:15:29 +0300

    lesson 1-2

---
+++ src/components/Profile.jsx
@@ -0,0 +1,38 @@
+import css from "./Profile.module.css";
+import PropTypes from "prop-types";
+
+export default function Profile({ username="Someone", tag="Someone`s tag", location="Somewhere", avatar="https://static.chipdip.ru/images/layout/noimage/230px.png", stats={followers: 0, views: 0, likes: 0} }){
+  return(
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} width="250px"/>
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers:</span>
+          <span className={css.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views:</span>
+          <span className={css.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes:</span>
+          <span className={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  )
+};
+
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.node,
+  stats: PropTypes.object
+}
